46fc02422a1a6bc203675d33bc3ff7e0f08b861e
owenswint <owenswint@billericak12.com>
2023-11-13 13:56:32 -0500

    11/13

---
+++ CondEx.java
@@ -0,0 +1,106 @@
+public class CondEx{
+    
+
+    public boolean isLeapYear(int year) {
+        boolean leap = false;
+        if (year%4 ==0){
+            leap = true;
+        }
+        if (year%100 ==0){
+            leap = false;
+        }
+        if (year%400 == 0){
+            leap = true;
+        }
+        return leap;
+    }
+
+    public boolean isSooner(int month1, int day1, int year1, int month2, int day2, int year2){
+        if (year1 != year2){
+            return year1<year2;
+        }
+        else if (month1 != month2){
+            return month1<month2;
+        }
+        else {
+            return day1<day2;
+        }
+    }
+
+
+    public int findBestFit(int space, int size1, int size2){
+        if ((size1+size2)<= space){
+            return 3;
+        }
+        else if (size2 > size1){
+            if (size2 <= space){
+                return 2;
+            }
+        }
+        else if (size1 <= space){
+            return 1;
+            }
+        else{
+            return 0;
+        }
+        return 0;  
+        
+        
+    }
+
+    public boolean makeBenches(int small, int big, int goal){
+        if ((small + big*5) >= goal){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args){
+        CondEx ce = new CondEx();
+        // leap year
+        System.out.println("2000 " + ce.isLeapYear(2000));
+        System.out.println("2004 " + ce.isLeapYear(2004));
+        System.out.println("2003 " + ce.isLeapYear(2003));
+        System.out.println("2100 " + ce.isLeapYear(2100));
+
+        // is sooner
+        System.out.println("isSooner");
+        System.out.println("1/2/2010 is sooner than 1/2/2011 " + ce.isSooner(1,2,2010, 1,2,2011));
+        System.out.println("2/1/2011 is sooner than 2/2/2010 " + ce.isSooner(2,1,2011, 2,2,2010));
+        System.out.println("1/2/2011 is sooner than 2/2/2010 " + ce.isSooner(1,2,2011, 2,2,2010));
+        System.out.println("1/5/2010 is sooner than 3/2/2010 " + ce.isSooner(1,5,2010, 3,2,2010));
+        System.out.println("3/2/2010 is sooner than 1/2/2010 " + ce.isSooner(3,2,2010, 1,2,2010));
+        System.out.println("1/3/2010 is sooner than 1/2/2010 " + ce.isSooner(1,3,2010, 1,2,2010));
+        System.out.println("5/2/2010 is sooner than 1/3/2011 " + ce.isSooner(5,2,2010, 1,3,2011));
+        System.out.println("1/2/2010 is sooner than 1/2/2010 " + ce.isSooner(1,2,2010, 1,2,2010));
+        System.out.println("4/8/2010 is sooner than 1/2/2010 " + ce.isSooner(4,8,2010, 1,2,2010));
+
+
+        // Best Fit
+        System.out.println("Best Fit");
+        System.out.println("Find Best fit 6 2 3 is " + ce.findBestFit(6,2,3));
+        System.out.println("Find Best fit 6 4 3 is " + ce.findBestFit(6,4,3));
+        System.out.println("Find Best fit 6 3 4 is " + ce.findBestFit(6,3,4));
+        System.out.println("Find Best fit 1 2 3 is " + ce.findBestFit(1,2,3));
+        System.out.println("Find Best fit 4 6 3 is " + ce.findBestFit(4,6,3));
+        System.out.println("Find Best fit 4 3 6 is " + ce.findBestFit(4,3,6));
+        System.out.println("Find Best fit 4 3 3 is " + ce.findBestFit(4,3,3));
+        System.out.println("Find Best fit 4 4 3 is " + ce.findBestFit(4,4,3));
+
+
+        // Make Bench
+        System.out.println("Make benches 3 1 8 is " + ce.makeBenches(3,1,8));
+        System.out.println("Make benches 3 1 9 is " + ce.makeBenches(3,1,9));
+        System.out.println("Make benches 11 1 15 is " + ce.makeBenches(11,1,15));
+        System.out.println("Make benches 4 2 15 is " + ce.makeBenches(4,2,15));
+        System.out.println("Make benches 20 0 20 is " + ce.makeBenches(20,0,20));
+        System.out.println("Make benches 3 4 20 is " + ce.makeBenches(3,4,20));
+        System.out.println("Make benches 0 6 30 is " + ce.makeBenches(0,6,30));
+        System.out.println("Make benches 0 5 30 is " + ce.makeBenches(0,5,30));
+        System.out.println("Make benches 2 6 23 is " + ce.makeBenches(2,6,23));
+        System.out.println("Make benches 3 2 23 is " + ce.makeBenches(3,2,23));
+
+    }
+}
---
+++ TwoSpinners.java
@@ -0,0 +1,53 @@
+public class TwoSpinners{
+ 	/** Precondition: min < max
+  	*/
+ 	/*The spin method simulates a spin of a fair spinner.
+  	* The method returns a random integer between min and max,
+  	* inclusive. Complete the spin method below by assigning
+  	* this random integer to result.
+  	*/
+ 	public int spin(int min, int max)
+ 	{
+     	    int result=0;
+     	    //add code here
+            result = math.random(min,max);
+    	    return result;
+ 	}
+
+ 	/** Simulates one round of the game as described in part (b).
+  	*/
+ 	public void playRound()
+ 	{
+   	    // add code here   
+        int player = spin(1,10);
+        int computer = spin(2,8);
+        if (player > computer){
+            System.out.println("You win! 3 points");
+        }
+        else if (computer > player){
+            System.out.println("You lose. -4 points");
+        }
+        else if (computer == player){
+            player = spin(1,10);
+            computer = spin(2,8);
+            if (player > computer){
+                System.out.println("You win! 1 points");
+            }
+            else if (computer > player){
+                System.out.println("You lose. -1 points");
+            }
+            else if (computer == player){
+                System.out.println("Tie. 0 points");
+            }
+        }
+	}
+
+ 	public static void main(String[] args) {
+     	  TwoSpinners ds = new TwoSpinners();
+     	      for(int i = 0; i < 10; i++){    //This will save you time by running playground 10 times
+         		ds.playRound();
+     	      }
+ 	  }
+
+ }
+

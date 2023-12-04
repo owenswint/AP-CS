cd2ebb585e20c19885f47bc285c74016384d0a44
owenswint <owenswint@billericak12.com>
2023-12-04 13:57:30 -0500

    12/4

---
+++ Whileloops.java
@@ -0,0 +1,86 @@
+public class Whileloops{
+
+    public int addOdds(int n){
+        int count = 1;
+        int sum = 0;
+        while (count <= n){
+            sum += count;
+            count += 2;
+            
+        }
+        return sum;
+    }
+
+     public int sumDigits(int n){
+        int place = 10;
+        int sum = 0;
+        while (n>=1){
+            sum += n%10;
+            n/= 10;
+        }
+        return sum;
+     }
+
+     public int howManyYears(double startpop, double endpop){
+        int years = 0;
+        while (startpop <= endpop){
+            startpop*=1.0113;
+            years+=1;
+        }
+        return years;
+     }
+
+     public void printSum(int n){
+        int sum = 0;
+        int printValue = 1;
+        while (n>= 1){
+            sum+=n;
+            n--;
+            if (printValue != 1){
+                System.out.print("+ ");
+            }
+            System.out.print(printValue + " ");
+            printValue++;
+            
+        }
+        System.out.println("= "+sum);
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public static void main(String[] args)
+  {
+    Whileloops wl = new Whileloops();
+    System.out.println(wl.addOdds(9));
+    System.out.println(wl.addOdds(0));
+    System.out.println(wl.addOdds(1));
+    System.out.println(wl.addOdds(67));
+    System.out.println(wl.addOdds(327));
+    System.out.println();
+    System.out.println(wl.sumDigits(183));
+    System.out.println(wl.sumDigits(887651324));
+    System.out.println(wl.sumDigits(3));
+    System.out.println(wl.sumDigits(16));
+    System.out.println(wl.sumDigits(675));
+    System.out.println();
+    System.out.println(wl.howManyYears(111.2, 120));
+    System.out.println(wl.howManyYears(111.2, 150));
+    System.out.println();
+    wl.printSum(6);
+    wl.printSum(8);
+
+
+
+  }
+}

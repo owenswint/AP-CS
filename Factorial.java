a468f8f1252e287c62d8c6cbfed62069932c20e5
owenswint <owenswint@billericak12.com>
2023-12-12 13:55:58 -0500

    12/12

--- Factorial.java
+++ Factorial.java
@@ -10,15 +10,32 @@ public class Factorial {
 
     }
 
-     public int calcE(int n){
-        int e = 
-        int olde = 
+     public double calcE(){
+        double e = 1;
+        double olde = 0; 
+        int count = 1;
         while(e-olde > .001){
-
+            olde = e;
+            e += 1.0/(calcFactorial(count));
+            count++;
         }
+        return e;
         
     }
-     }
+
+    public double calcEX(int x){
+        double ex = 1;
+        double oldex = 0; 
+        int count = 1;
+        while(ex-oldex > .001){
+            oldex = ex;
+            ex += (Math.pow(x,count))/(calcFactorial(count));
+            count++;
+        }
+        return ex;
+    }
+
+     
 
 
 
@@ -27,6 +44,15 @@ public class Factorial {
     for(int i = 1; i <= 20; i++){
         System.out.println(f.calcFactorial(i));
     }
+    System.out.println();
+    
+    System.out.println("e is %2.3f \n", f.calcE());
+    System.out.println();
+
+    for(int i = 1; i<= 5; i++){
+        System.out.println(f.calcEX(i));
+    }
+    
    
   }
 

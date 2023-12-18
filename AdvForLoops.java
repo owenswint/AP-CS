daef4dc99e7375b5c907149485f199afe5371a85
owenswint <owenswint@billericak12.com>
2023-12-18 13:56:29 -0500

    12/18

---
+++ AdvForLoops.java
@@ -0,0 +1,16 @@
+public class AdvForLoops {
+    
+    public void eights(){}
+
+    public void alternate(int h){}
+
+    public void isosceles(int h){}
+
+    public void upSideDown(int start, int n){}
+
+
+
+    public static void main(String[] args){
+        AdvForLoops f = new AdvForLoops();    
+    }
+}
--- ForLoops.java
+++ ForLoops.java
@@ -10,13 +10,74 @@ public class ForLoops {
         return sum;
     }
 
-    public void printSquresBlack(int n){
-        for(int i = n; i >= 0; i--){
-            int squared = i^2;
-            System.out.println(squared + ", "));
+    public void printSquaresBack(int n){
+        for(int i = n; i > 0; i--){
+            int squared = i*i;
+            if (i>1){
+                System.out.print(squared + ", ");
+            }
+            else{
+                System.out.print(squared);
+            }
+        }
+        System.out.println();
+    }
+
+
+    public void sillyNumbers(){
+        
+        for(int t = 1; t<= 3; t++){
+            for(int i = 0; i <= 9; i++){
+            for(int j = 1; j <= 3; j++){
+                System.out.print(i);
+            }
+        }
+        System.out.println();
         }
     }
 
+    public void sillyNumbers2(){
+        
+        for(int t = 1; t<= 3; t++){
+            for(int i = 9; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print(i);
+            }
+        }
+        System.out.println();
+        }
+    }
+
+
+    public void dollarsAndStars(){
+        int dollarnum = 7;
+        int staredge = 0;
+        int starmid = 14;
+
+        for(int i = 1; i<= 7; i++){
+
+            for(int a = 1; a <= staredge; a++){
+                System.out.print("*");
+            }
+            for(int a = 1; a <= dollarnum; a++){
+                System.out.print("$");
+            }
+            for(int a = 1; a <= starmid; a++){
+                System.out.print("*");
+            }
+            for(int a = 1; a <= dollarnum; a++){
+                System.out.print("$");
+            }
+            for(int a = 1; a <= staredge; a++){
+                System.out.print("*");
+            }
+            
+            dollarnum --;
+            staredge += 2;
+            starmid -= 2;
+            System.out.println();
+        } 
+    }
 
     public static void main(String[] args){
         ForLoops f = new ForLoops();
@@ -24,9 +85,25 @@ public class ForLoops {
         System.out.println(f.addEvens(8));
         System.out.println(f.addEvens(3));
         System.out.println(f.addEvens(12));
-        System.out.println(f.addEvens(7));
+        System.out.println(f.addEvens(103));
+        System.out.println();
 
+        f.printSquaresBack(8);
+        f.printSquaresBack(10);
+        f.printSquaresBack(2);
         f.printSquaresBack(7);
-        // fix squares method
+        System.out.println();
+
+        f.sillyNumbers();
+        System.out.println();
+
+        f.sillyNumbers2();
+        System.out.println();
+
+        f.dollarsAndStars();
+        System.out.println();        
+
+
+        
     }
 }

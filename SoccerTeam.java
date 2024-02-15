bf9dd2bd5169b6e2ed088ca0dc2d8f09d4b702ba
owenswint <owenswint@billericak12.com>
2024-02-15 13:43:17 -0500

    2/15

--- SoccerTeam.java
+++ SoccerTeam.java
@@ -13,6 +13,8 @@ public class SoccerTeam{
      
     public void played(SoccerTeam other, int myScore, int otherScore){
         gamesPlayed ++;
+        goalsScored += myScore;
+        goalsScored += otherScore;
         if(myScore == otherScore){
             this.ties ++;
             other.ties ++;
@@ -55,6 +57,55 @@ public class SoccerTeam{
         SoccerTeam Chelmsford = new SoccerTeam(0, 2, 5);
         SoccerTeam Burlington = new SoccerTeam(3, 0, 2);
         SoccerTeam Andover = new SoccerTeam(5, 2, 0);
+         
+
+
+        // Tournament 1
+
+        startTournament();
+        Billerica.played(Chelmsford, 5, 2); 
+        Billerica.played(Burlington, 2, 2); 
+        Andover.played(Chelmsford, 1, 4); 
+        Burlington.played(Andover, 2, 1); 
+
+        System.out.println("Tournament 1");
+        System.out.println("Billerica: " + Billerica.getPoints() + " points");
+        System.out.println("Chelmsford: " + Chelmsford.getPoints() + " points");
+        System.out.println("Burlington: " + Burlington.getPoints() + " points");
+        System.out.println("Andover: " + Andover.getPoints() + " points");
+        System.out.println("Total Games Played: "+ SoccerTeam.getTotalGamesPlayed());
+        System.out.println("Total Goals Scored: "+ SoccerTeam.getTotalGoalsScored());
+        System.out.println();
+
+        Billerica.reset();
+        Burlington.reset();
+        Chelmsford.reset();
+        Andover.reset();
+
+
+        // Tournament 2
+
+        startTournament();
+        Andover.played(Chelmsford, 1, 6); 
+        Billerica.played(Burlington, 5, 3); 
+        Andover.played(Chelmsford, 1, 0); 
+        Burlington.played(Billerica, 3, 3); 
+
+        System.out.println("Tournament 2");
+        System.out.println("Billerica: " + Billerica.getPoints() + " points");
+        System.out.println("Chelmsford: " + Chelmsford.getPoints() + " points");
+        System.out.println("Burlington: " + Burlington.getPoints() + " points");
+        System.out.println("Andover: " + Andover.getPoints() + " points");
+        System.out.println("Total Games Played: "+ SoccerTeam.getTotalGamesPlayed());
+        System.out.println("Total Goals Scored: "+ SoccerTeam.getTotalGoalsScored());
+
+        Billerica.reset();
+        Burlington.reset();
+        Chelmsford.reset();
+        Andover.reset();
+
+        
+    }
 
 
 
